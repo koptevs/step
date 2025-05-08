c2107ddfd37d83184b9c216ed5cd3cdcc875dd58
Igors Koptevs <igors.koptevs@gmail.com>
2025-05-08 17:52:29 +0300

    Fixed error by  eslint-disable-next-line @typescript-eslint/no-unused-vars

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,5 +1,6 @@
 'use client'; // Error boundaries must be Client Components
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function GlobalError({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
     return (
         // global-error must include html and body tags
