d676b8f7f4789026559ce9c7d5a9c03e4ba10aa9
Igors Koptevs <igors.koptevs@gmail.com>
2025-05-08 17:36:39 +0300

    Added dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+import type { Metadata } from 'next';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/mode-toggle';
+
+export const metadata: Metadata = {
+    title: 'Home',
+};
 
 export default function Home() {
     return (
         <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
             <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+                <ModeToggle />
+                <Button>
+                    <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
+                    shadcn/iu
+                </Button>
                 <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
                 <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
                     <li className="mb-2 tracking-[-.01em]">
@@ -35,10 +46,6 @@ export default function Home() {
                     >
                         Read our docs
                     </a>
-                    <Button>
-                        <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-                        shadcn/iu
-                    </Button>
                 </div>
             </main>
             <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,33 @@
+import { Geist, Geist_Mono, Inter, Lusitana } from 'next/font/google';
+import localFont from 'next/font/local';
+
+export const inter = Inter({
+    variable: '--font-inter',
+    subsets: ['latin'],
+});
+
+export const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+});
+
+export const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export const notoSansRegular = localFont({
+    src: './local/Noto-Sans-regular.woff',
+    variable: '--font-noto-sans-regular',
+
+    // weight: "100 900",
+});
+
+// in any file:
+// import { lusitana } from '@/app/ui/fonts';
+// <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`} >
